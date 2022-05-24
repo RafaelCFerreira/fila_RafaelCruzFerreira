d07487494ed34ecc9c80a04038ad98a57ee95d52
RafaelCFerreira <rafaelcferreira232@gmail.com>
2022-05-24 18:05:04 -0300

    att

--- fila.c
+++ fila.c
@@ -7,9 +7,9 @@
 int main(){
     int option;
     char aux;
-    struct sfila filaCampos, filaGuarus;
-    qinit(&filaCampos);
-    qinit(&filaGuarus);
+    struct sfila filaCampos_Guarus, filaGuarus_Campos;
+    qinit(&filaCampos_Guarus);
+    qinit(&filaGuarus_Campos);
     do{
         printf("1 - Cadastro Campos -> Guarus\n");
         printf("2 - Cadastro Guarus -> Campos\n");
@@ -19,42 +19,44 @@ int main(){
         scanf("%d", &option);
         system("clear");
         switch (option){
-            case 1: if(count(&filaCampos) < MAX){
+            case 1: if(count(&filaCampos_Guarus) < MAX){
                 printf("\nEntre com a placa do carro: ");
                 scanf(" %c", &aux);
-                enqueue(&filaCampos, aux);
-                printf("Primeiro carro: %c\n", first(&filaCampos));
-                printf("Último carro: %c\n", last(&filaCampos));
-                printf("Total de carros: %d\n\n", count(&filaCampos));
+                enqueue(&filaCampos_Guarus, aux);
+                printf("Primeiro carro: %c\n", first(&filaCampos_Guarus));
+                printf("Último carro: %c\n", last(&filaCampos_Guarus));
+                printf("Total de carros: %d\n\n", count(&filaCampos_Guarus));
             }else{
                 printf("Fila cheia!!!\n\n");
             }
             break;
-            case 2: if(count(&filaGuarus) < MAX){
+            case 2: if(count(&filaGuarus_Campos) < MAX){
                 printf("\nEntre com a placa do carro: ");
                 scanf(" %c", &aux);
-                enqueue(&filaGuarus, aux);
-                printf("Primeiro carro: %c\n", first(&filaGuarus));
-                printf("Último carro: %c\n", last(&filaGuarus));
-                printf("Total de carros: %d\n\n", count(&filaGuarus));
+                enqueue(&filaGuarus_Campos, aux);
+                printf("Primeiro carro: %c\n", first(&filaGuarus_Campos));
+                printf("Último carro: %c\n", last(&filaGuarus_Campos));
+                printf("Total de carros: %d\n\n", count(&filaGuarus_Campos));
             }else{
                 printf("Fila cheia!!!\n\n");
             }
 	        break;
-            case 3: printf("\n* Travessia liberada! *\n");
-                printf("Sentido Campos-Guarus\n");
-                if(!qisEmpty(&filaCampos)){
-                    printf("\tAtravessando carro: %c\n\n", first(&filaCampos));
-                    dequeue(&filaCampos);
+            case 3: printf("\nTravessia liberada! \n");
+                printf("Sentido Campos->Guarus\n");
+                if(!qisEmpty(&filaCampos_Guarus)){
+                    printf("Atravessando carro: %c\n\n", first(&filaCampos_Guarus));
+                    dequeue(&filaCampos_Guarus);
+                    system("sleep 5");
                 }else{
-                    printf("\tNao há carros para atravesar.\n");
+                    printf("Nenhum carro para atravesar.\n");
                 }
-                printf("Sentido Guarus-Campos\n");
-                if(!qisEmpty(&filaGuarus)){
-                    printf("\tAtravessando carro: %c\n\n", first(&filaGuarus));
-                    dequeue(&filaGuarus);
+                printf("Sentido Guarus->Campos\n");
+                if(!qisEmpty(&filaGuarus_Campos)){
+                    printf("Atravessando carro: %c\n\n", first(&filaGuarus_Campos));
+                    dequeue(&filaGuarus_Campos);
+                    system("sleep 5");
                 }else{
-                    printf("\tNao há carros para atravesar.\n");
+                    printf("Nenhum carro para atravesar.\n");
                 }
             break;
         }
